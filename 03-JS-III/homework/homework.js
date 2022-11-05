16d628eb36fb7dd626fbe5539d44ab969f3f721a
Edward V <ervasalloe@unal.edu.co>
2022-11-05 14:27:05 -0500

    third load

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,54 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    var total=0;
+    for (var i = 0                 ; i <= 10                 ; i++          ) {
+      // | Declaramos una variable | Expresión condcicional | Incrementamos la variable |
+      total=total+i;
+      }
+      return total;
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  var pares=[];
+  for(numero of array){
+    if(numero%2==0){
+      pares.push(numero)//Agrega <numero> al array <pares>
+    }
+  }
+  return pares;
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  var cuadrados=[];
+  for(numero of array){
+  cuadrados.push(numero**2)//Agrega <numero> al array <pares>
+  }
+  return cuadrados;
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  var suma=0;
+  for(numero of array){
+    suma=suma+numero;
+  }
+  return suma;
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  numletter=num.toString();
+  return(numletter.length);
 }
   
   
